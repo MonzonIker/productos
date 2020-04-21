929e0995b0c01299614072ab347208f604838709
Monzon <Monzon@DESKTOP-NGNHNN7>
2020-04-21 11:31:51 +0200

    21/04/2020

--- src/api/ApiDeleteProducto.java
+++ src/api/ApiDeleteProducto.java
@@ -1,11 +1,15 @@
 package api;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloProducto;
 
 /**
  * Servlet implementation class ApiDeleteProducto
@@ -27,7 +31,16 @@ public class ApiDeleteProducto extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int id = Integer.parseInt(request.getParameter("id"));
+		try {
+            ModeloProducto mProducto = new ModeloProducto();
+            mProducto.delete(id);
+            mProducto.getConexion().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		
 	}
 
 	/**
